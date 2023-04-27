fe6f140a79ac54c90b0cc635c0ad86e2e5e2daeb
weihsinyeh <weihsinyeh168@gmail.com>
2023-04-27 10:27:30 +0800

    Include JSON header file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
-#include
+#include <iostream>
+
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+using namespace rapidjson;
+int main(){
+// JSON string
+const char *json = "{\"name\":\"Joe\",\"grade\":\"A\"}";
+// Parsing the JSON string into DOM.
+Document DOM;
+DOM.Parse(json);
+// Stringifying the DOM
+StringBuffer buffer;
+Writer<StringBuffer> writer(buffer);
+DOM.Accept(writer);
+std::string completeJsonData = buffer.GetString();
+std::cout << completeJsonData << std::endl;
+return 0;
+}
